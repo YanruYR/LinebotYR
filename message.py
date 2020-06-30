1c2650d8d45abad691dbe7e128a26977b3163ae0
YanruYR <YanruYR@users.noreply.github.com>
2020-06-30 17:22:03 +0800

    crystalframe1

--- message.py
+++ message.py
@@ -110,7 +110,7 @@ def Carousel_Template():
             columns=[
                 CarouselColumn(
                     thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Number_1_in_green_rounded_square.svg/200px-Number_1_in_green_rounded_square.svg.png',
-                    title='這是第一塊模板',
+                    title='crystalframe',
                     text='一個模板可以有三個按鈕',
                     actions=[
                         PostbackTemplateAction(
@@ -129,7 +129,7 @@ def Carousel_Template():
                 ),
                 CarouselColumn(
                     thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
-                    title='這是第二塊模板',
+                    title='使用說明',
                     text='副標題可以自己改',
                     actions=[
                         PostbackTemplateAction(
@@ -148,7 +148,7 @@ def Carousel_Template():
                 ),
                 CarouselColumn(
                     thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png',
-                    title='這是第三個模塊',
+                    title='如何購買',
                     text='最多可以放十個',
                     actions=[
                         PostbackTemplateAction(
